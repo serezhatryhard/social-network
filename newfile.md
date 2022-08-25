e1c9cc51d654202b3103e2acbacaa744fe506579
serezhatryhard <serezhad41@gmail.com>
2022-08-25 18:56:14 +0500

    upload to github

---
+++ newfile.md
@@ -0,0 +1,1 @@
+#I made new file
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <main className='content'>
+            Main content
+            <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEBx9Wr0-Vrvo7-X_EwAXnCxBrBODj3sjPLE_6DZPA&s' /></div>
+            <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLJJ-BAkyVtBk2vKpkFf7yjELiyctbPFAMHUOjh6W_YQ&s' /></div>
+        </main>
+    )
+}
+
+export default Profile
